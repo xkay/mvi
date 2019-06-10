cefc8b5e4164619baba61bc7a3b84e6381f76e08
Kay Chen <kchen@apartmentlist.com>
2019-06-09 21:38:18 -0700

    Add required functions to Model

--- Model.swift
+++ Model.swift
@@ -6,19 +6,31 @@ protocol Model: class {
     associatedtype ACTION: Equatable
     
     var disposables: CompositeDisposable { get }
-    var viewModel: ReplaySubject<VM> { get }
+    var viewModel: BehaviorSubject<VM> { get }
     var events: PublishSubject<Event> { get }
     var actions: PublishSubject<ACTION> { get }
-    
-    /**
-     Attach the model to an intent stream
-     */
+
+    /// Attach the model to an intent stream
+    ///
+    /// Usage:
+    ///    override func viewDidLoad() {
+    ///        super.viewDidLoad()
+    ///        attach(viewModel: model.getViewModel(), actions: model.getActions())
+    ///        model.attach(intents: getIntents())
+    ///    }
     func attach(intents: Observable<INTENT>)
     
-    /**
-     Detach the model from previously attached intent stream
-     */
+    /// Detach the model from previously attached intent stream
     func detach()
+    
+    /// Create new model with original viewmodel and
+    func reduce(model: VM, event: Event) -> VM
+    
+    /// Map Intent to Event
+    func eventsFrom(intents: Observable<INTENT>) -> Observable<Event>
+
+    /// Map Intent to Action
+    func actionsFrom(intents: Observable<INTENT>) -> Observable<ACTION>
 }
 
 extension Model {
